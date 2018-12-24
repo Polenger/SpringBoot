6f790c1a056963407cfaffbe3e248588b57a6dee
loikun <Polenger@users.noreply.github.com>
2018-12-24 13:41:07 +0800

    更新

--- SpringBoot-Mybatis/src/main/java/com/loikun/boot/BootApplication.java
+++ SpringBoot-Mybatis/src/main/java/com/loikun/boot/BootApplication.java
@@ -3,13 +3,14 @@ package com.loikun.boot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Repository;
 
 /**
  * 启动类
  * @author loikun
  */
 @SpringBootApplication
-@MapperScan(basePackages = "com.loikun.boot.dao") // 映射文件扫描
+@MapperScan(basePackages = "com.loikun.boot.dao",annotationClass = Repository.class) // 映射文件扫描
 public class BootApplication {
 
     public static void main(String[] args) {
