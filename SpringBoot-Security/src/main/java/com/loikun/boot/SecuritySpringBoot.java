3655ec03c1dd7fe8d2361e48e616b846c30a8069
loikun <Polenger@users.noreply.github.com>
2018-12-25 15:49:41 +0800

    Spring Security初步

---
+++ SpringBoot-Security/src/main/java/com/loikun/boot/SecuritySpringBoot.java
@@ -0,0 +1,15 @@
+package com.loikun.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 采用Spring 的认证逻辑
+ */
+@SpringBootApplication
+public class SecuritySpringBoot {
+
+    public static void main(String[] args){
+        SpringApplication.run(SecuritySpringBoot.class, args);
+    }
+}
